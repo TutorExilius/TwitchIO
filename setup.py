8ba6afcd546c646252729d0ea67d24f2abb57862
SnowyLuma <SnowyLuma@gmail.com>
2018-10-21 06:16:35 +0200

    Clean up setup.py, add extra requirements for server package

--- setup.py
+++ setup.py
@@ -24,66 +24,77 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-import os
 import re
-from setuptools import setup
+import subprocess
 
+import setuptools
 
-on_rtd = os.getenv('READTHEDOCS') == 'True'
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-if on_rtd:
-    requirements.append('sphinx==1.7.4')
-    requirements.append('sphinxcontrib-napoleon')
-    requirements.append('sphinxcontrib-asyncio')
-    requirements.append('sphinxcontrib-websupport')
-    requirements.append('Pygments')
-
-version = ''
 with open('twitchio/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
-    raise RuntimeError('version is not set')
+    raise RuntimeError('TwitchIO version is not set.')
 
-# Thanks Dannyo(Rapptz) again...
+# Thanks Danny(Rapptz) again...
 if version.endswith(('a', 'b', 'rc')):
-    # append version identifier based on commit count
     try:
-        import subprocess
-        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # append version identifier based on commit count
+        p = subprocess.Popen(
+            ['git', 'rev-list', '--count', 'HEAD'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
         out, err = p.communicate()
         if out:
             version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        p = subprocess.Popen(
+            ['git', 'rev-parse', '--short', 'HEAD'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
         out, err = p.communicate()
         if out:
             version += '+' + out.decode('utf-8').strip()
     except Exception:
         pass
 
-if not version:
-    raise RuntimeError('version is not set')
-
-readme = ''
 with open('README.rst') as f:
     readme = f.read()
 
-setup(name='twitchio',
-      author='EvieePy',
-      url='https://github.com/EvieePy/TwitchIO',
-      version=version,
-      packages=['twitchio', 'twitchio.ext.commands'],
-      license='MIT',
-      description='A python IRC and API wrapper for Twitch.',
-      long_description=readme,
-      include_package_data=True,
-      install_requires=requirements,
-      classifiers=[
+extras_require = {
+    'docs': (
+        'pygments',
+        'sphinx==1.7.4',
+        'sphinxcontrib-asyncio',
+        'sphinxcontrib-napoleon',
+        'sphinxcontrib-websupport',
+    ),
+    'server': (
+        'sanic',
+    ),
+}
+
+setuptools.setup(
+    name='twitchio',
+    author='TwitchIO',
+    url='https://github.com/TwitchIO/TwitchIO',
+    version=version,
+    packages=['twitchio', 'twitchio.ext.commands', 'twitchio.ext.server'],
+    license='MIT',
+    description='A Python API and IRC wrapper for Twitch.',
+    long_description=readme,
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=requirements,
+    extras_require=extras_require,
+    classifiers=[
         'Development Status :: 2 - Beta',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
